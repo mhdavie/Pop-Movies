dc5e2a1697d35cbeabf1dccd495289db3c7248ff
Mariana Hodniki Davie <marianahodniki1991@gmail.com>
2022-05-31 11:22:47 -0600

    added key and fuctio to fecth data

--- script.js
+++ script.js
@@ -6,7 +6,24 @@ const BASE_URL = 'https://api.themoviedb.org/3/'
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'
 + API_KEY ;
 
+getMovies(API_URL);
 
+function getMovies(url) {
+    fetch(url).then(res => res.json).then(data => {
+        console.log(data);
+    })
+}
+
+
+function ShowMovie(data) {
+
+    data.forEach(movie => {
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie')
+        movieEl.innerHTML = ''
+
+    })
+}
 
 
 

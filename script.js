47d328de98cea2003f766f5b46127b27e1409e40
Mariana Hodniki Davie <marianahodniki1991@gmail.com>
2022-05-31 11:12:50 -0600

    starting api key

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const API_KEY = 'api_key=48057a0fa3f9c2e1cfcc9e05fcb49758';
+
+const BASE_URL = 'https://api.themoviedb.org/3/'
+
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'
++ API_KEY ;
+
+
+
+
+
